7f036db26f9f7c58dfb98d0735464842191fc257
malak <malak.sayour@lau.edu>
2024-02-20 15:25:07 +0200

    align merged

--- include/agents.h
+++ include/agents.h
@@ -19,7 +19,7 @@
 int agent_number=4;
 std::mutex mtx_;
 //std::map<int, octomap_msgs::msg::Octomap> octomapDictionary;
-std::map<int, double> filterDictionary={{1,0.5},{2,0.2},{3,0.5},{4,0.5},{5,0.5},{6,0.5},{7,0.5}};
+std::map<int, double> filterDictionary={{0,0.5},{1,0.5},{2,0.2},{3,0.5},{4,0.5},{5,0.5},{6,0.5},{7,0.5}};
 std::string offline_map_path="";
 int offline_map_id=4;
 //std::map<int, Eigen::Matrix4f> tfDictionary;
@@ -27,7 +27,7 @@ int offline_map_id=4;
 //sensor_msgs::msg::PointCloud2 add_pcl;
 //sensor_msgs::msg::PointCloud2 delete_pcl;
 float mapResolution=0.05;
-std::vector<float> agentWeights={1.5, 2, 3,1,1};
+std::vector<float> agentWeights={1.5, 2, 3,1,1,1,1};
 
 
 //wa2tiye
